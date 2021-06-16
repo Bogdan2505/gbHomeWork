2edf4ad246ddfccdd8b100462b1dad2c006416a2
Bogdan <bogbog680@gmail.com>
2021-06-16 22:15:09 +0300

    выполнил задание к уроку 1 ("Java. Ведение")

--- src/dz1/HomeWorkApp.java
+++ src/dz1/HomeWorkApp.java
@@ -2,6 +2,50 @@ package dz1;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-        
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 5;
+        int b = 2;
+        int c = a + b;
+
+        if (c >= 0) {
+            System.out.println("сумма положениительная");
+        } else {
+            System.out.println("сумма отрицицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 3;
+
+        if (value <= 0) {
+            System.out.println("красный");
+        } else if (value > 0 && value < 101) {
+            System.out.println("желтый");
+        } else {
+            System.out.println("заленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 0;
+        int b = 5;
+
+        if (a >= b){
+            System.out.println("a >=b");
+        }else {
+            System.out.println("a < b");
+        }
     }
 }
